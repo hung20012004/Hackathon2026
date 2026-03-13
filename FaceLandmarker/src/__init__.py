7f88692a3bf81d1ed49d8a1bd487297a97e8f7f8
hnmhung <hamanhhung20012004@gmail.com>
2026-03-13 09:57:06 +0000

    update

--- FaceLandmarker/src/__init__.py
+++
@@ -1,4 +0,0 @@
-from .face_detector import FaceLandmarker, FaceLandmarks
-from .affine_normalizer import AffineNormalizer, OUTPUT_SIZE
-
-__all__ = ["FaceLandmarker", "FaceLandmarks", "AffineNormalizer", "OUTPUT_SIZE"]
